0b5c9a575ec8921a88cbcbb3a9cef9ad7b4a8f23
Ricardo Sprocati <ricardosprocatioliveira@outlook.com>
2024-08-24 19:15:14 -0300

    componentes finalizados

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,66 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+import Cabecalho from './components/Cabecalho.vue';
+import Formulario from './components/Formulario.vue';
+import ListaDeTarefas from './components/ListaDeTarefas.vue';
+
+const estado = reactive({
+  filtro: 'todas',
+  tarefaTemporaria:"",
+  tarefas: [
+    {
+      titulo: 'Estudar ES6',
+      finalizada: false,
+    },
+    {
+      titulo: 'Estudar SASS',
+      finalizada: false,
+    },
+    {
+      titulo: 'Jogar Bola',
+      finalizada: true,
+    },
+  ]
+})
+
+const getTarefasPendentes =() => {
+  return estado.tarefas.filter(tarefa => tarefa.finalizada === false)
+}
+
+const getTarefasFinalizadas =() => {
+  return estado.tarefas.filter(tarefa => tarefa.finalizada === true)
+}
+
+const getTarefasFiltradas = () => {
+  const {filtro} = estado;
+
+  switch (filtro) {
+    case 'pendentes':
+      return getTarefasPendentes();
+    case 'finalizadas':
+      return getTarefasFinalizadas();
+    default:
+      return estado.tarefas;
+  }
+}
+
+const cadastrarTarefa = () => {
+  const tarefaNova = {
+    titulo: estado.tarefaTemporaria,
+    finalizada: false,
+  }
+  estado.tarefas.push(tarefaNova);
+  estado.tarefaTemporaria= '';
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <Cabecalho :tarefas-pendentes="getTarefasPendentes().length"/>
+    <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temporaria="estado.tarefaTemporaria" :edita-tarefa-temp="evento => estado.tarefaTemporaria = evento.target.value" :cadastrar-tarefa="cadastrarTarefa" />
+    <ListaDeTarefas :tarefas="getTarefasFiltradas()"/>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,22 @@
+<script setup>
+    const props = defineProps(['cadastrarTarefa', 'tarefaTemporaria', 'trocarFiltro', 'editaTarefaTemp'])
+</script>
+
+<template>
+    <form class="row" @submit.prevent="props.cadastrarTarefa">
+        <div class="col">
+            <input :value="props.tarefaTemporaria" @change="$props.editaTarefaTemp"
+                required type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+        </div>
+        <div class="col-md-2">
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+        <div class="col-md-2">
+            <select @change="$props.trocarFiltro" class="form-control">
+                <option value="todas">Todas tarefas</option>
+                <option value="pendentes">Pendentes</option>
+                <option value="finalizadas">Finalizadas</option>
+            </select>
+        </div>
+    </form>
+</template>
